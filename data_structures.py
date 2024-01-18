dafa141bf0a110fb65a15ff2cd0da58092803ece
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-01-18 17:22:07 +0200

    main information

---
+++ data_structures.py
@@ -0,0 +1,103 @@
+#Collections
+collection = '''a program object (container variable) that stores a set of values of one or different types.'''
+
+# The main collection types in Python :
+
+lists='''ordered collections that can contain elements of any type. 
+Lists are mutable, which means you can change, add, or remove items after they are created.'''
+
+tuples = '''similar to lists, but they are immutable. This means that once a tuple is created, it cannot be modified. 
+Tuples are often used to store data that should not change while the program is running.'''
+
+dictionaries = '''collections of key-value pairs where each key is unique. 
+Dictionaries are used when quick access to elements by key is required.'''
+
+sets = '''unordered collections of unique items. 
+Used to remove duplicates and perform operations that are typical 
+for mathematical sets, such as union, intersection, difference.'''
+
+frozen_sets = '''these are invariant versions of sets. 
+Such sets cannot be changed after they are created.'''
+
+# list methods
+
+chars = ['a', 'b', 'c']
+numbers = [1, 2]
+chars.extend(numbers)
+chars.insert(1, "b")
+c_ind = chars.index('b')
+chars_copy=chars.copy()
+copy_chars = chars[:]
+count=chars.count('b')
+first = chars.pop(-1)
+last=chars.remove(1)
+chars.sort(reverse=True)
+chars.reverse()
+reverse_chars = chars[::-1]
+chars.clear() # []
+print(dir(list))
+
+# dict methods
+
+info = {'name': 'Julia', 'age': 14, 'city': 'Kharkiv', 'email': 'usere@example.com'}
+print("name" in info)
+del info['city']
+info.pop('email')
+info.update({'color':"yellow","number":67})
+new_info=info.copy()
+# without KeyError if key is't in dict
+level = info.get("level")
+# caused KeyError
+# level = info["level"]
+info.clear()
+print(dir(dict))
+
+# set methods
+lst = [1, 2, 3, 1, 2, 2, 3, 4, 1]
+d_lst = set(lst)
+d_lst.add(5)
+d_lst.remove(3)
+d_lst.discard(3)
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a.intersection(b))  # {3}
+print(a & b)  # {3}
+print(a.difference(b))  # {1, 2}
+print(a - b)  # {1, 2}
+print(a.symmetric_difference(b))  # {1, 2, 4, 5}
+print(a ^ b)  # {1, 2, 4, 5}
+print(a.union(b))  # {1, 2, 3, 4, 5}
+print(a | b)  # {1, 2, 3, 4, 5}
+print(dir(set))
+
+my_frozenset = frozenset([1, 2, 3, 4, 5])
+print(my_frozenset)
+print(dir(frozenset))
+
+# tuple methods
+my_tuple = tuple(lst)
+my_tuple.count(1)
+my_tuple.index(2)
+print(my_tuple)
+print(dir(tuple))
+
+# string method
+s = "Responsive Mockup of an iMac with a White iPad and iPhone"
+s.upper()
+s.lower()
+s.title()
+s.capitalize()
+s.isalpha()
+
+# unpacking lists and dictionaries
+my_list = [1, 2, 3]
+a, b, c = my_list
+a, _, c = my_list
+a, *rest = my_list
+print(*rest)
+
+def greet(name, age):
+    print(f"Hello {name}, you are {age} years old.")
+
+person_info = {"name": "Alice", "age": 25}
+greet(**person_info)
---
+++ functions.py
@@ -0,0 +1,109 @@
+# Parameters to a function are some inputs that we can pass 
+# to the function in order to get a result corresponding to that data.
+
+
+
+# Functions can take parameters, that is, some values passed 
+# inside the function for it to do something with. 
+# These parameters are similar to variables, except 
+# that the values of these variables are specified 
+# when the function is called, and they are already 
+# assigned their values when the function runs.
+
+
+
+# Parameters are specified in parentheses when declaring
+# a function and are separated by commas. 
+# Similarly, we pass a value when we call a function.
+
+
+
+# Note the terminology: the names you specify 
+# when you declare a function are called parameters,
+# while the values you pass to the function 
+# when you call it are called arguments.
+
+def print_max(a, b):
+    if a > b:
+        print(a, 'max')
+    elif a == b:
+        print(a, 'equal', b)
+    else:
+        print(b, 'max')
+
+print_max(3, 4)  # direct transfer of values
+
+x = 5
+y = 7
+print_max(x, y)  # passing variables as arguments
+
+# type hints
+def add_numbers(num1: int, num2: int) -> int:
+    sum = num1 + num2
+    return sum
+
+result = add_numbers(5, 10)
+print(result)  
+
+def is_even(num: int) -> bool:
+    return num % 2 == 0
+
+check_even = is_even(4)
+print(check_even) 
+
+# A scope is an area in your program (code) within which 
+# you can refer to the contents of a variable by name. 
+# These scopes are divided into four levels in the search order 
+# of variable names, and are known as the LEGB rule:
+
+
+
+L = '''Local: This is the internal level where the name is 
+defined inside a function or block of code.'''
+E = '''Enclosing: This is the scope that encloses the local scope. 
+If a function is inside another function, the names defined 
+in the enclosing function will be available to the inner function.'''
+G = '''Global: This is module or script level scope. 
+Variables defined at this level are available throughout the module.'''
+B = '''Built-in: This is the outermost level that contains names built into Python. 
+For example, we considered built-in functions, len, range, etc.'''
+
+
+
+recursion = ''' is a concept in programming where a function calls itself 
+within its own execution. It's like breaking a big problem down into smaller, more manageable problems.'''
+
+def fibonacci(n):
+     if n <= 1: # base case
+         return n
+     else:
+         return fibonacci(n-1) + fibonacci(n-2) # recursive case
+
+print(fibonacci(10)) # will output 55
+
+call_stack = '''is a specific part of memory used to store information
+about active function calls. Each time a function is called, a new entry
+(or "layer") is created on this stack for that particular call.
+This layer contains information about the function's variables,
+its parameters, and the location from where the function was called, 
+so that when the function completes, the program can continue from the correct location.'''
+
+def factorial(n):
+     print("Calling the factorial function with n = ", n)
+     if n == 1:
+         print("Base case, n = 1, return 1")
+         return 1
+     else:
+         result = n * factorial(n-1)
+         print("Return result for n = ", n, ": ", result)
+         return result
+
+print(factorial(5))
+
+# Recursive functions are convenient in situations where we do not know 
+# in advance how many times the function will need to be called, 
+# for example, when parsing directories on a disk. The application 
+# does not know in advance how deep the directory structure is and 
+# what level of nesting they have. And to iterate over all files 
+# in all nested directories, the function must call itself when 
+# it encounters the next directory.
